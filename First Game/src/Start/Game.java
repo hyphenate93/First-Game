7aa946ac6d135817abacb0b9845f45bb924ff627
oscand18 <oscar.andersson@hotmail.se>
2019-05-09 14:20:47 +0200

    suck a dick

--- First Game/src/Start/Game.java
+++ First Game/src/Start/Game.java
@@ -15,7 +15,7 @@ public class Game extends Canvas implements Runnable {
 	private Handler handler;
 	private Random r;
 	private HUD hud;
-
+	// andreas suger
 	public Game() {
 
 		handler = new Handler();
